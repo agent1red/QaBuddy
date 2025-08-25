2531621c3b62001fa277d9a8bcde9befb3e1989a
agent1red <darklen14@gmail.com>
2025-08-25 12:50:04 -0700

    Initial Commit

---
+++ QaBuddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QaBuddy
+//
+//  Created by Kevin Hudson on 8/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QaBuddy/QaBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QaBuddyApp.swift
+//  QaBuddy
+//
+//  Created by Kevin Hudson on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct QaBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
